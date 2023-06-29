7e0c3c4ab94fac0bb18f22dd89360687a44b9c57
mikelawtonn <138080140+mikelawtonn@users.noreply.github.com>
2023-06-29 10:56:09 +0100

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,10 +1,3 @@
-#Game of blackjack where the ace is 11 and its going to count as 11 until the user goes over 21. We also assume we have
-#an infinite depth so it means that when a card is drawn from the deck it is not removed from the deck where as in a
-#casino they have maybe 6 or 8 decks of cards and when a card is drawn there is less probability of that card being drawn again.
-#However we want to keep things simple so we assume an infinite deck. REMMEBER THE DEALER KEEPS DRAWING CARDS UNTIL 17 OR
-#ABOVE IS REACHED.
-
-
 #importing random module
 import random
 
